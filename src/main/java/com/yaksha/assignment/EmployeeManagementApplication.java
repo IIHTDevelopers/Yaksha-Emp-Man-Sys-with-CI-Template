f0e8e2d5a1ec4af43449023b1b708370bd106944
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-16 08:54:41 +0530

    Update EmployeeManagementApplication.java

--- src/main/java/com/yaksha/assignment/EmployeeManagementApplication.java
+++ src/main/java/com/yaksha/assignment/EmployeeManagementApplication.java
@@ -6,13 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
-		// Load the Spring context from the XML configuration file
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		// Retrieve the Employee bean from the Spring container
-		Employee employee = context.getBean("employee1", Employee.class);
-
-		// Display employee details
-		employee.displayEmployeeDetails();
+		// write your logic here
 	}
 }
